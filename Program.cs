6f39c23539b2273bbd84003b80c8bc9a31efa968
Saar Mazor <saarmz.910@gmail.com>
2023-05-23 11:28:08 +0300

    Added usage example

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+
+namespace Proxy
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Proxy.RunProxyAsync(2109);
+        }
+    }
+}
